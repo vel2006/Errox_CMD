ce7ccab7ccd743c7d0d7d742d43654878dd7fdb4
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-11-27 21:52:03 -0600

    Delete InstallPython.bat

--- InstallPython.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set PythonVersion=3.12.4
-set PythonInstaller=https://www.python.org/ftp/python/%PythonVersion%/python-%PythonVersion%-amd64.exe
-set InstallerPath=%TEMP%\python-installer.exe
-powershell -Command "Invoke-WebRequest -Uri '%PythonInstaller%' -OutFile '%InstallerPath%'"
-%InstallerPath% /quiet InstallAllUsers=1 PrependPath=1
