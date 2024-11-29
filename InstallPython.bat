a3dc58d343651a71640ba6f5b48bed2f5869b4a4
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-11-28 18:58:06 -0600

    Update InstallPython.bat

--- InstallPython.bat
+++ InstallPython.bat
@@ -1,6 +1,15 @@
 @echo off
-set PythonVersion=3.12.4
-set PythonInstaller=https://www.python.org/ftp/python/%PythonVersion%/python-%PythonVersion%-amd64.exe
-set InstallerPath=%TEMP%\python-installer.exe
-powershell -Command "Invoke-WebRequest -Uri '%PythonInstaller%' -OutFile '%InstallerPath%'"
-%InstallerPath% /quiet InstallAllUsers=1 PrependPath=1
+echo Seeing if python is installed
+python --version >nul
+if %errorlevel% neq 0 (
+    echo Python is not installed, installing
+    set PythonVersion=3.12.4
+    set PythonInstaller=https://www.python.org/ftp/python/%PythonVersion%/python-%PythonVersion%-amd64.exe
+    set InstallerPath=%TEMP%\python-installer.exe
+    powershell -Command "Invoke-WebRequest -Uri '%PythonInstaller%' -OutFile '%InstallerPath%'"
+    %InstallerPath% /quiet InstallAllUsers=1 PrependPath=1
+    echo python should be installed now
+) else (
+    echo Python is installed, version below
+    python --version
+)
