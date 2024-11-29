02d676e608de81c7fbea71f1e2c153fcb3e3b956
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-11-28 18:58:42 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -8,11 +8,12 @@ echo ^|_____^|_^|  ^|_^|  \___/_/\_\___\____^|_^|  ^|_^|____/
 echo                         ^|_____^|
 echo.
 echo                      Created By: That1EthicalHacker
-echo                                        Version: 2.0
+echo                                        Version: 2.1
 echo.
 :: Starting basic info grabbing for a lay of the land
 echo Starting a gather of information for a lay of the land
 call GatherSystemInfo.bat
+echo Running InstallPython.bat script
+call InstallPython.bat
 echo Starting a batch shell to bypass a block of cmd.exe
 call BatchTerminal.bat
-call InstallPython.bat
