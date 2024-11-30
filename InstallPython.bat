bb3c49215f432d55548ebd9d8ed2cd0dfd28c50e
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-11-29 20:28:34 -0600

    Update InstallPython.bat
    Fixing the script so it actually works (in theory)

--- InstallPython.bat
+++ InstallPython.bat
@@ -3,13 +3,17 @@ echo Seeing if python is installed
 python --version >nul
 if %errorlevel% neq 0 (
     echo Python is not installed, installing
-    set PythonVersion=3.12.4
-    set PythonInstaller=https://www.python.org/ftp/python/%PythonVersion%/python-%PythonVersion%-amd64.exe
-    set InstallerPath=%TEMP%\python-installer.exe
-    powershell -Command "Invoke-WebRequest -Uri '%PythonInstaller%' -OutFile '%InstallerPath%'"
-    %InstallerPath% /quiet InstallAllUsers=1 PrependPath=1
+    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.4/python-%PythonVersion%-amd64.exe' -OutFile '%TEMP%\python-installer.exe'"
+    %InstallerPath% InstallAllUsers=1 PrependPath=1
     echo python should be installed now
 ) else (
     echo Python is installed, version below
     python --version
+)
+echo Testing for python...
+python --version >nul
+if %errorlevel% neq 0 (
+    echo Out of luck it seems.
+) else (
+    echo Python installed!
 )
