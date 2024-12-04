296fecf912a7633875931c185e9e03b4214b383a
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-12-04 12:06:53 -0600

    Update InstallPython.bat
    Fixing a typo in the Invoke-WebRequest line that would cause the uri to not be found

--- InstallPython.bat
+++ InstallPython.bat
@@ -3,7 +3,7 @@ echo Seeing if python is installed
 python --version >nul
 if %errorlevel% neq 0 (
     echo Python is not installed, installing
-    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.4/python-%PythonVersion%-amd64.exe' -OutFile '%TEMP%\python-installer.exe'"
+    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe' -OutFile '%TEMP%\python-installer.exe'"
     %InstallerPath% InstallAllUsers=1 PrependPath=1
     echo python should be installed now
 ) else (
