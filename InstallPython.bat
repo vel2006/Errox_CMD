8e6fc61274fb516767939e0ff32ad3e4e6005256
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-12-19 22:56:11 -0600

    Update InstallPython.bat
    Fixing some basic typos that are out of date (hopefully work now)

--- InstallPython.bat
+++ InstallPython.bat
@@ -3,10 +3,8 @@ echo Seeing if python is installed
 python --version >nul
 if %errorlevel% neq 0 (
     echo Python is not installed, installing
-    set PythonVersion=3.12.4
-    set PythonInstaller=https://www.python.org/ftp/python/%PythonVersion%/python-%PythonVersion%-amd64.exe
-    set InstallerPath=%TEMP%\python-installer.exe
-    powershell -Command "Invoke-WebRequest -Uri '%PythonInstaller%' -OutFile '%InstallerPath%'"
+    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.4/python-%PythonVersion%-amd64.exe' -OutFile 'python-installer.exe'"
+    set InstallPath=python-installer.exe
     %InstallerPath% /quiet InstallAllUsers=1 PrependPath=1
     echo python should be installed now
 ) else (
